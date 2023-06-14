1c6b6536846ba96ef22c31138caf49c1e3e1812f
yoonseo232 <ys.bang232@gmail.com>
2023-06-14 18:52:09 +0900

    .

--- test.js
+++
@@ -1,6 +0,0 @@
-var _showPage = function() {
-    var loader = $("div.loader");
-    var container = $("div.container");
-    loader.css("display","none");
-    container.css("display","block");
-  };
